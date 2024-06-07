6f60c08c404f4a367c37d15baedbc02a5dd217cf
Sharvin-M <smanjrekar2@ucmerced.edu>
2024-06-07 09:36:57 -0700

    scraper returns dict of links:projects

--- scraper.py
+++ scraper.py
@@ -1,1 +1,22 @@
-import requestshtml
+import requests
+from bs4 import BeautifulSoup as bs
+
+url = 'http://nifty.stanford.edu/'
+
+page = requests.get(url)
+soup = bs(page.content, "html.parser")
+
+all_links = soup.find_all('a')
+text = []
+tag = []
+for i in range(2, len(all_links)):
+    if (all_links[i].text == '(Video)' or all_links[i].text == 'PDF version'):
+        continue
+    text.append(all_links[i].text)
+    tag.append(all_links[i].get('href'))
+print(text)
+print(tag)
+
+tag_text = { k:v for (k,v) in zip(tag, text)}  
+
+print(tag_text)
