5a95a8dbd7f8c575a9d22732e0279b9f737ce950
Sharvin-M <smanjrekar2@ucmerced.edu>
2024-06-10 11:57:56 -0700

    dealing with CORS issue

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const apiURL = ''
+
+const apiURL = "https://5xuj7ap5ok.execute-api.us-west-1.amazonaws.com/prod/";
 
 function nifty() {
-
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
+  };
+  xhttp.open(
+    "GET",
+    "https://5xuj7ap5ok.execute-api.us-west-1.amazonaws.com/prod/",
+    true
+  );
+  xhttp.send();
 }
