a59ed812be3795bc16fe85a75350b37cf238faf7
Sharvin-M <smanjrekar2@ucmerced.edu>
2024-06-11 22:45:42 -0700

    did as much styling as I had patience for

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ function nifty() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      console.log(this.responseText);
+      const res = this.responseText.split(",");
       const projectElement = document.getElementById("projects");
-      const project = document.createElement("p");
-      project.innerText = this.responseText;
+      const nameElement = document.getElementById("name");
+
+      projectElement.innerHTML = "";
+      nameElement.innerHTML = "";
+
+      const project = document.createElement("p2");
+      const name = document.createElement("p1");
+      project.innerText = res[0].replace(/"$/, "").replace(/^\["/, "");
+      name.innerText = res[1].replace(/^"/, "").replace(/"]$/, "");
+
       projectElement.appendChild(project);
+      nameElement.appendChild(name);
+
+      projectElement.style.display = "block";
+      nameElement.style.display = "block";
     }
   };
   xhttp.open(
