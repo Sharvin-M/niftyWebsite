b218f6ab28daac79fca4a29d02617e27499ed9a0
Sharvin-M <smanjrekar2@ucmerced.edu>
2024-06-20 19:00:01 -0700

    added concurrency

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ handler = Mangum(app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
 
